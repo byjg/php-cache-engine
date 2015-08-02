9b33f9a1042aae8133e7ed648d1383398e28ac8e
Joao Gilberto Magalhaes <joao@byjg.com.br>
2015-08-02 19:04:55 -0300

    Fix example.php

--- example.php
+++ example.php
@@ -2,13 +2,20 @@
 
 require "vendor/autoload.php";
 
-$cacheEngine = \ByJG\Cache\CacheContext::factory();
+ob_start();
 
+
+// Get the 'default' object in the config/cacheconfig.php
+$cacheEngine = \ByJG\Cache\CacheContext::factory();
 var_dump(\ByJG\Cache\CacheContext::getInstance()->getMemcachedConfig());
 
-// ------------------
+// Use SessionCacheEngine
+$sessionCache = new ByJG\Cache\SessionCacheEngine();
+$sessionCache->set('key', 'somevalue');
+echo $sessionCache->get('key') . "\n";
 
-$cacheTest = \ByJG\Cache\FileSystemCacheEngine::getInstance();
+// Attach log to cache
+$cacheTest = new \ByJG\Cache\FileSystemCacheEngine();
 
 ByJG\Cache\LogHandler::getInstance()->pushLogHandler(new \Monolog\Handler\StreamHandler('php://stderr', Monolog\Logger::DEBUG));
 
