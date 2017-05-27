1d97d34d3fde42277653802a3588670a035710be
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-05-27 12:59:02 -0300

    Fixed TTL in ArrayCacheEngine

--- src/Engine/ArrayCacheEngine.php
+++ src/Engine/ArrayCacheEngine.php
@@ -21,12 +21,29 @@ class ArrayCacheEngine extends BaseCacheEngine
     }
 
     /**
-     * @param string $key
+     * Determines whether an item is present in the cache.
+     * NOTE: It is recommended that has() is only to be used for cache warming type purposes
+     * and not to be used within your live applications operations for get/set, as this method
+     * is subject to a race condition where your has() will return true and immediately after,
+     * another script can remove it making the state of your app out of date.
+     *
+     * @param string $key The cache item key.
      * @return bool
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
      */
     public function has($key)
     {
-        return isset($this->cache[$key]);
+        if (isset($this->cache[$key])) {
+            if (isset($this->cache["$key.ttl"]) && time() >= $this->cache["$key.ttl"]) {
+                $this->delete($key);
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -46,16 +63,27 @@ class ArrayCacheEngine extends BaseCacheEngine
     }
 
     /**
-     * @param string $key The object Key
-     * @param object $value The object to be cached
-     * @param int $ttl The time to live in seconds of this objects
-     * @return bool If the object is successfully posted
+     * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
+     *
+     * @param string                $key   The key of the item to store.
+     * @param mixed                 $value The value of the item to store, must be serializable.
+     * @param null|int|DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
+     *                                     the driver supports TTL then the library may set a default value
+     *                                     for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
      */
-    public function set($key, $value, $ttl = 0)
+    public function set($key, $value, $ttl = null)
     {
         $this->logger->info("[Array cache] Set '$key' in L1 Cache");
 
         $this->cache[$key] = $value;
+        if (!empty($ttl)) {
+            $this->cache["$key.ttl"] = $this->addToNow($ttl);
+        }
 
         return true;
     }
@@ -74,6 +102,7 @@ class ArrayCacheEngine extends BaseCacheEngine
     public function delete($key)
     {
         unset($this->cache[$key]);
+        unset($this->cache["$key.ttl"]);
         return true;
     }
 
