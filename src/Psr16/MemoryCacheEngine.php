3d9cb2896a3b61616443a0be9fe6886add8561dd
Joao Gilberto Magalhaes <joao@byjg.com.br>
2024-12-18 10:02:46 -0600

    Add MemoryCacheEngine.php

---
+++ src/Psr16/MemoryCacheEngine.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ByJG\Cache\Psr16;
+
+use ByJG\Cache\CacheLockInterface;
+use ByJG\Cache\Exception\InvalidArgumentException;
+use DateInterval;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Log\LoggerInterface;
+
+class MemoryCacheEngine extends FileSystemCacheEngine
+{
+
+    public function __construct(?LoggerInterface $logger = null)
+    {
+        parent::__construct('cache', '/dev/shm', $logger);
+    }
+}
